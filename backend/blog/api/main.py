b33eab5fbf54f34ab4744f23e261e1b4a9b49438
lazarodu <lazarodu@gmail.com>
2025-06-20 19:19:41 -0300

    sem api

--- backend/blog/api/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from blog.api.routes import user, post, comment
-
-app = FastAPI(title="Blog API", version="1.0.0")
-
-app.include_router(user.router, prefix="/users", tags=["Users"])
-app.include_router(post.router, prefix="/posts", tags=["Posts"])
-app.include_router(comment.router, prefix="/comments", tags=["Comments"])
